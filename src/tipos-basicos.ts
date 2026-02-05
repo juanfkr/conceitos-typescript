7a5c2fecf0f96b9c6b32f5fdf5759616153c90d1
juan ferreira <juanferreiraverao@gmail.com>
2026-02-04 20:15:05 -0400

    initial commit

---
+++ src/tipos-basicos.ts
@@ -0,0 +1,14 @@
+export {};
+
+// tipos
+let name: string = "Juan";
+let age: number = 19;
+let isActive: boolean = true;
+let data: null = null;
+let value: undefined = undefined;
+
+// inferencia de tipos
+let phoneNumber: number = 1031235 // phoneNumber só recebe valores do tipo *number*
+
+console.log(name, age, isActive, data, value, phoneNumber)
+
